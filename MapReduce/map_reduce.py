110aa7a71922541dce7b7951943310b0527b1bac
Simone Trubian <simone.trubian@gmail.com>
2015-10-04 16:58:41 +0100

    Finished first generating function.

--- MapReduce/map_reduce.py
+++ MapReduce/map_reduce.py
@@ -1,8 +1,13 @@
 from string import ascii_letters
 from random import randrange, choice
 
+
+def genLine() -> str:
+    return ''.join((choice(ascii_letters) for _ in
+                    range(randrange(3, 15)))) + '\n'
+
+
 def genFile(count: int) -> None:
     with open('list.txt', 'w') as out_file:
         for line in range(count):
-            out_file.write(''.join( (choice(ascii_letters) for _ in
-                range(randrange(3,15)))) + '\n')
+            out_file.write(genLine())
