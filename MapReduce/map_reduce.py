53f476dd6f0ff9243781d2cd7759c585fc0b4ac1
Simone Trubian <simone.trubian@gmail.com>
2015-10-06 21:37:12 +0100

    Corrected a bug in the get_file function and implemented the built in call chain.

--- MapReduce/map_reduce.py
+++ MapReduce/map_reduce.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from string import ascii_letters
 from random import randrange, choice
 from typing import List, Iterator
@@ -20,14 +21,19 @@ def genFile(count: int) -> None:
 # -------------------------------------------------------------------------- #
 #           Vanilla in-memory list.
 # -------------------------------------------------------------------------- #
-def getFile(file_path: str) -> List[str]:
+def get_file(file_path: str) -> List[str]:
     with open(file_path, 'r') as in_file:
-        return in_file.read()
+        str_list = []
+        for line in in_file:
+            str_list.append(line)
+
+        return str_list
 
 def mapping_fun(in_str: str) -> int:
     return len([char for char in in_str if char.isupper()])
 
-def for_red(in_list: Iterator[int]) -> int:
+#def for_red(in_list: Iterator[int]) -> int: FIXME: make this fun generic.
+def for_red(in_list):
     sum = 0
     for count in in_list: sum += count
     return sum
@@ -51,10 +57,26 @@ def gen_map(in_list: Iterator[str]) -> Iterator[int]:
     return (mapping_fun(row) for row in in_list)
 
 # -------------------------------------------------------------------------- #
+#           Built-in functions.
+# -------------------------------------------------------------------------- #
+def buil_fil(in_list: List[str]) -> Iterator[str]:
+    return filter(lambda row: len(row) > 3 and len(row) < 15, in_list)
+
+def buil_map(in_list: Iterator[str]) -> Iterator[int]:
+    #import ipdb; ipdb.set_trace()
+    return map(lambda row: mapping_fun(row), in_list)
+
+def buil_red(in_list: Iterator[int] )-> int:
+    return reduce(lambda nex, prev: nex + prev, in_list)
+
+# -------------------------------------------------------------------------- #
 #           Callable functions.
 # -------------------------------------------------------------------------- #
 def comp_sum() -> int:
-    return for_red(comp_map(comp_fil(getFile(FILE_PATH))))
+    return for_red(comp_map(comp_fil(get_file(FILE_PATH))))
 
 def gen_sum() -> int:
-    return for_red(gen_map(gen_fil(getFile(FILE_PATH))))
+    return for_red(gen_map(gen_fil(get_file(FILE_PATH))))
+
+def buil_sum() -> int:
+    return buil_red(buil_map(buil_fil(get_file(FILE_PATH))))
