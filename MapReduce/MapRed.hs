be599e53617f5e32dad5a4f1507a429d5620b729
Simone Trubian <simone.trubian@gmail.com>
2015-10-11 16:11:54 +0100

    Optimised single core running time.

--- MapReduce/MapRed.hs
+++ MapReduce/MapRed.hs
@@ -1,2 +1,21 @@
+import Data.Char (isUpper)
+import Data.List (foldl1')
+
 filterStr :: [String] -> [String]
 filterStr = filter (\s -> (length s > 3) && (length s < 15))
+
+capCount :: String -> Int
+capCount = foldl addUpper 0
+
+addUpper :: Int -> Char -> Int
+addUpper count char = if isUpper char then count + 1 else count
+
+totCaps :: [String] -> Int
+--totCaps = sum . map capCount . filterStr
+totCaps = foldl1' (+) . map capCount . filterStr
+
+
+main :: IO ()
+main = do
+    file <- readFile "list.txt"
+    print $ totCaps $ lines file
