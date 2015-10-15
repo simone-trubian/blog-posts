68055dea40f8b531777c6606d14bc37f29c57a04
Simone Trubian <simone.trubian@gmail.com>
2015-10-15 07:22:01 +0100

    Added test parallel haskell module.

--- MapReduce/time.hs
+++ MapReduce/time.hs
@@ -1,6 +1,7 @@
 import Data.List (foldl1')
 import System.Environment (getArgs)
 import Control.Parallel.Strategies (rpar, rseq, Eval, runEval)
+import Control.DeepSeq (force)
 
 sumInt :: [Int] -> Int
 sumInt = foldl1' (+)
@@ -9,8 +10,8 @@ run :: Int -> Int
 run n = runEval $ do
     let ns = [1..n]
         (a', b') = splitAt (length ns `div` 2) ns
-    a <- rpar $ sumInt a'
-    b <- rpar $ sumInt b'
+    a <- rpar $ (force sumInt a')
+    b <- rpar $ (force sumInt b')
     rseq a
     rseq b
     return (a + b)
