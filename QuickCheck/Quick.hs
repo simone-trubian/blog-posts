885116d9d0575e35fea83e6aeb4b5ef222ed5c83
Simone Trubian <simone.trubian@gmail.com>
2015-10-28 21:38:59 +0000

    Created a sum recursive function and wrote props for it.

--- QuickCheck/Quick.hs
+++ QuickCheck/Quick.hs
@@ -1,7 +1,16 @@
 import Test.QuickCheck (quickCheck)
 import Data.List (sort)
 
+sum' :: [Int] -> Int
+
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+propSumAsso :: [Int] -> Bool
+propSumAsso xs = sum' xs + sum' xs == sum' (xs ++ xs)
+
+propSumComm :: [Int] -> Bool
+propSumComm xs = sum' xs == sum' (reverse xs)
+
 propIdem :: [Int] -> Bool
 propIdem xs = sort xs == sort (sort xs)
-
-main = quickCheck propIdem
