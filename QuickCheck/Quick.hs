3f8f63481aeda4d8ad2dd02161b2ab9ba1472cd5
Simone Trubian <simone.trubian@gmail.com>
2015-10-29 07:19:51 +0000

    Created a partially true property.

--- QuickCheck/Quick.hs
+++ QuickCheck/Quick.hs
@@ -12,5 +12,8 @@ propSumAsso xs = sum' xs + sum' xs == sum' (xs ++ xs)
 propSumComm :: [Int] -> Bool
 propSumComm xs = sum' xs == sum' (reverse xs)
 
+propSumPos :: [Int] -> Bool
+propSumPos xs = sum' xs >= 0
+
 propIdem :: [Int] -> Bool
 propIdem xs = sort xs == sort (sort xs)
