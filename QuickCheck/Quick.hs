bb1a66f38a430e0705c033693220e492ddb835ba
Simone Trubian <simone.trubian@gmail.com>
2015-11-02 21:43:26 +0000

    Trying various Modifiers type signature strategies.

--- QuickCheck/Quick.hs
+++ QuickCheck/Quick.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Test.QuickCheck
   (
    quickCheck
   ,verboseCheck
   ,shrink
   )
+
+import Test.QuickCheck.Modifiers (Positive (..))
 import Data.List (sort)
 
-sum' :: [Int] -> Int
+--sum' :: [Int] -> Int
 
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
@@ -17,8 +21,13 @@ propSumAsso xs = sum' xs + sum' xs == sum' (xs ++ xs)
 propSumComm :: [Int] -> Bool
 propSumComm xs = sum' xs == sum' (reverse xs)
 
-propSumPos :: [Int] -> Bool
-propSumPos xs = sum' xs >= 0
+propSumPos :: (Num a, Show a, Ord a) => Positive [a] -> Bool
+--propSumPos :: Positive [Int] -> Bool
+--propSumPos = undefined
+--propSumPos (Positive (xs :: [Num]) ) = sum' xs >= 0
+propSumPos (Positive (xs :: [a]) ) = sum' xs >= 0
+
+prop2 = propSumPos :: Positive [Int] -> Bool
 
 propIdem :: [Int] -> Bool
 propIdem xs = sort xs == sort (sort xs)
