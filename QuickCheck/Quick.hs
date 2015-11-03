413a01293da33032dea553bd49d4491c1a558b02
Simone Trubian <simone.trubian@gmail.com>
2015-11-03 21:28:16 +0000

    Created a workin implementation of prop_SumPos .

--- QuickCheck/Quick.hs
+++ QuickCheck/Quick.hs
@@ -1,6 +1,7 @@
 -- Try commenting out the two following pragmas to fail the typeclass instance.
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
 
 import Test.QuickCheck
   (
@@ -28,13 +29,16 @@ import Test.QuickCheck.Gen
 
 import Data.List (sort)
 
-type PosList = [Int]
+type PosList = [Integer] --Int overflows the test!
 
-instance Arbitrary (PosList) where
+-- List can overflow Int
+instance Arbitrary PosList where
   arbitrary = sized $ \n -> do
     k <- choose (0, n)
-    list <- sequence [ arbitrary | _ <- [1..k] ] -- Make it generate only > 0
-    return $ list
+    list <- sequence [ arbitrary | _ <- [1..k] ]
+    return $ filter (> 0) list
+
+sum' :: Num a => [a] -> a
 
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
