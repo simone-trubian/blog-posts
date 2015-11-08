d6b14e4ede8234e7eddf9053cd8d97ad0d868519
Simone Trubian <simone.trubian@gmail.com>
2015-11-08 17:45:25 +0000

    Changed phone number generation implementation and removed property implementation.

--- QuickCheck/Arb.hs
+++ QuickCheck/Arb.hs
@@ -3,13 +3,10 @@ import Test.QuickCheck
    quickCheck
   ,verboseCheck
   ,shrink
-  ,elements
   ,suchThat
   ,arbitrary
-  ,sized
   ,choose
-  ,shuffle
-  ,sublistOf
+  ,sample
   ,Gen (..)
   ,Arbitrary (..)
   )
@@ -19,14 +16,12 @@ data Phone = Phone String String String
 
 instance Arbitrary Phone where
   arbitrary = do
-    c <- elements ["0044", "0011", "0039"]
-    p <- elements ["0207", "0208"]
-    x <- choose (10000, 999999) :: Gen Int
-    let n = show x
-    return (Phone c p n)
-
+    countryCode <- choose (1, 999) :: Gen Int
+    areaCode <- choose (1, 9999) :: Gen Int
+    phoneNumber <- choose (10000, 999999) :: Gen Int
+    return (Phone (show countryCode) (show areaCode) (show phoneNumber))
 
 genPhone :: Gen Phone
 genPhone = arbitrary
 
-prop_numLen (Phone a b c) = length a == length b && length b <= length c
+prop_numLen (Phone a b c) = undefined
