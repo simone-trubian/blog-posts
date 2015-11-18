99363bbcc21cb41bef14cf4fb8b0a5143b65c4c4
Simone Trubian <simone@avidglobalmedia.com>
2015-11-18 15:18:39 +0000

    Removed comments from files.

--- Require/app.js
+++ Require/app.js
@@ -1,6 +1,3 @@
-// Configure loading modules from the lib directory,
-// except for 'app' ones, which are in a sibling
-// directory.
 requirejs.config({
     baseUrl: 'node_modules',
     paths: {
@@ -8,6 +5,4 @@ requirejs.config({
     }
 });
 
-// Start loading the main app file. Put all of
-// your application logic in there.
 requirejs(['app/main']);
--- Require/app/main.js
+++ Require/app/main.js
@@ -1,13 +1,4 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    //var messages = require('./messages');
-
-    // Load library/vendor modules using
-    // full IDs, like:
-    //var print = require('print');
-
     var paragraph = document.createElement('p');
     var node = document.createTextNode('I just loaded stuff!');
     paragraph.appendChild(node);
