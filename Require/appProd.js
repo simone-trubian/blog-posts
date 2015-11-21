7eb0bc5f09f0ef6bf289709917ca88dff3ab554b
Simone Trubian <simone.trubian@gmail.com>
2015-11-21 11:00:53 +0000

    Added a production version of the imports.

---
+++ Require/appProd.js
@@ -0,0 +1,11 @@
+// Configure paths so that can be used throughout the project.
+requirejs.config({
+    baseUrl: 'node_modules',
+    paths: {
+        main: '../app/main',
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min'
+    }
+});
+
+// Load the app by importing the 'main' module.
+requirejs(['main']);
