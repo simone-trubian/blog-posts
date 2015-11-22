5f36f11aa9f8b69ec51bb4d7bf580df4e160c4ac
Simone Trubian <simone.trubian@gmail.com>
2015-11-22 19:45:00 +0000

    Using google compiler to minify, works in simple, breaks require import in advanced.

--- Require/src/build.js
+++ Require/src/build.js
@@ -3,6 +3,12 @@
     baseUrl: 'app',
     dir: '../build',
     fileExclusionRegExp: /^node_modules/,
+    optimize: 'closure',
+    closure: {
+        CompilerOptions: {},
+        CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+        loggingLevel: 'TRACE'
+    },
     paths: {
         jquery: 'empty:'
     },
