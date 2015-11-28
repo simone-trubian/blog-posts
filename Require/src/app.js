a54ddf67d6847196b6e91e0ab90cdac6b73b5b69
Simone Trubian <simone.trubian@gmail.com>
2015-11-28 13:48:43 +0000

    Addedd shim's for all angular dependencies.

--- Require/src/app.js
+++ Require/src/app.js
@@ -4,7 +4,22 @@ requirejs.config({
     paths: {
         app: '../app',
         home: '../app/home',
-        angular: 'angular/angular/angular',
+        angular: 'angular/angular',
+        ngRoute: 'angular-route/angular-route',
+        ngResource: 'angular-resource/angular-resource',
+    },
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'ngRoute': {
+            deps: ['angular'],
+            exports: 'ngRoute'
+        },
+        'ngResource': {
+            deps: ['angular'],
+            exports: 'ngResource'
+        }
     }
 });
 
